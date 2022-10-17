d992ea2b7cf42c8dd4cc0ca91eab6a3dbb74edd1
chelsealau <100821409+chelsealau@users.noreply.github.com>
2022-10-17 00:00:10 -0400

    Add files via upload

---
+++ project2/sentiment_analyzer.py
@@ -0,0 +1,33 @@
+import tweepy
+from main import authenticate
+from textblob import TextBlob
+import statistics
+from typing import List
+
+# Get tweets using twitter API that are returned from searching for keyword
+def retrieve_tweets(keyword: str) -> List[str]:
+    api = authenticate()
+    tweet_list = []
+    for tweet in tweepy.Cursor(api.search, q=keyword, tweet_mode='extended', lang='en').items(100):
+        tweet_list.append(tweet.full_text)
+    return tweet_list
+
+# Calculate sentiment for each tweet returned using textblob
+def calc_sentiment(cleaned_tweets: List[str]) -> List[float]:
+    score_list = []
+    for tweet in cleaned_tweets:
+        blob = TextBlob(tweet)
+        score_list.append(blob.sentiment.polarity)
+    return score_list
+
+# Return average sentiment
+def avg_sentiment(keyword: str) -> int:
+    tweets = retrieve_tweets(keyword)
+    sentiment_scores = calc_sentiment(tweets)
+    avg_sent = statistics.mean(sentiment_scores)
+
+    return avg_sent
+
+if __name__ == '__main__':
+    keyword = input("KEYWORD TO SEARCH: ")
+    print(avg_sentiment(keyword))
